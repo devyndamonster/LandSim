fbda221481ccf12e9f98a29671912d12d71f0d10
Devyn Myers <devyndamonster@hotmail.com>
2023-09-02 20:15:23 -0400

    Added perlin noise grid

---
+++ LandSim/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace LandSim.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> collection)
+        {
+            var random = new Random();
+            return collection.OrderBy(item => random.Next());
+        }
+    }
+}
