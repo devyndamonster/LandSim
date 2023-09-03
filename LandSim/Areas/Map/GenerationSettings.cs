2468854835297d287163bfd482f0afee61047172
Devyn Myers <devyndamonster@hotmail.com>
2023-09-03 10:43:12 -0400

    Added settings and fixed row col bug

---
+++ LandSim/Areas/Map/GenerationSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace LandSim.Areas.Map
+{
+    public class GenerationSettings
+    {
+        public string Seed { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+    }
+}
