b9d51329af3256abe2075358ea88c9b7821d3994
Devyn Myers <devyndamonster@hotmail.com>
2023-09-07 18:38:09 -0400

    Moved terrain generation into service

---
+++ LandSim/Areas/Map/Models/TerrainTile.cs
@@ -0,0 +1,17 @@
+﻿using LandSim.Areas.Map.Enums;
+
+namespace LandSim.Areas.Map.Models
+{
+    public class TerrainTile
+    {
+        public int TerrainTileId { get; set; }
+
+        public int XCoord { get; set; }
+
+        public int YCoord { get; set; }
+
+        public TerrainType TerrainType { get; set; }
+
+        public float Height { get; set; }
+    }
+}
